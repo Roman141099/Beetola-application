17e00a883a1449072a8bea492750befc666990f8
Your Name <roma.grigoriev.2015@inbox.ru>
2023-02-14 01:15:45 +0400

    Refactored services

--- src/main/java/org/beetola/app/service/CurrencyConversionService.java
+++ src/main/java/org/beetola/app/service/CurrencyConversionService.java
@@ -1,6 +1,11 @@
 package org.beetola.app.service;
 
+import lombok.RequiredArgsConstructor;
+import org.beetola.app.config.CurrencyConversionConfig;
+import org.beetola.app.exception.CurrencyException;
+import org.beetola.app.model.domain.CurrencyRate;
 import org.beetola.app.model.dto.CurrencyConversionRs;
+import org.beetola.app.repository.CurrencyRateRepository;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.DecimalMin;
@@ -8,14 +13,32 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.valueOf;
+import static org.beetola.app.util.CurrencyUtil.*;
+import static org.beetola.app.util.MessageUtil.buildCurrencyNotAvailableMsg;
+
 @Validated
-public interface CurrencyConversionService {
+@RequiredArgsConstructor
+public abstract class CurrencyConversionService {
 
-    CurrencyConversionRs exchangeToSellSome(@NotNull @Pattern(regexp = "[A-Z]{3}") String from,
-                                            @NotNull @Pattern(regexp = "[A-Z]{3}") String to,
-                                            @NotNull @DecimalMin("1") BigDecimal sellAmount);
+    private final CurrencyRateRepository baselineRepository;
+    private final CurrencyConversionConfig currencyConfig;
 
-    CurrencyConversionRs exchangeToBySome(@NotNull @Pattern(regexp = "[A-Z]{3}") String from,
-                                          @NotNull @Pattern(regexp = "[A-Z]{3}") String to,
-                                          @NotNull @DecimalMin("1") BigDecimal buyAmount);
+    public CurrencyConversionRs exchange(@NotNull @Pattern(regexp = "[A-Z]{3}") String from,
+                                         @NotNull @Pattern(regexp = "[A-Z]{3}") String to,
+                                         @NotNull @DecimalMin("1") BigDecimal amount) {
+        CurrencyRate fromRate = baselineRepository.findById(from)
+                .orElseThrow(() -> new CurrencyException(buildCurrencyNotAvailableMsg(from)));
+        CurrencyRate toRate = baselineRepository.findById(to)
+                .orElseThrow(() -> new CurrencyException(buildCurrencyNotAvailableMsg(to)));
+        BigDecimal coefficient = getCoefficient(toRate.getCoefficient(), fromRate.getCoefficient());
+
+        BigDecimal result = coefficient.multiply(amount).setScale(GLOBAL_DECIMAL_SCALE, GLOBAL_ROUNDING_MODE);
+        BigDecimal commission = result.multiply(valueOf(currencyConfig.getMarginPercent()))
+                .setScale(GLOBAL_DECIMAL_SCALE, GLOBAL_ROUNDING_MODE);
+        return buildResult(result, commission);
+    }
+
+    protected abstract CurrencyConversionRs buildResult(BigDecimal result, BigDecimal commission);
+
 }
---
+++ src/main/java/org/beetola/app/service/impl/conversion/CurrencyConversionServiceToSell.java
@@ -0,0 +1,27 @@
+package org.beetola.app.service.impl.conversion;
+
+import org.beetola.app.config.CurrencyConversionConfig;
+import org.beetola.app.model.dto.CurrencyConversionRs;
+import org.beetola.app.repository.CurrencyRateRepository;
+import org.beetola.app.service.CurrencyConversionService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service("serviceToSell")
+public class CurrencyConversionServiceToSell extends CurrencyConversionService {
+
+    public CurrencyConversionServiceToSell(CurrencyRateRepository baselineRepository,
+                                           CurrencyConversionConfig currencyConfig) {
+        super(baselineRepository, currencyConfig);
+    }
+
+    @Override
+    public CurrencyConversionRs buildResult(BigDecimal result, BigDecimal commission) {
+        BigDecimal finalCost = result.subtract(commission);
+        return new CurrencyConversionRs()
+                .summary("")
+                .amount(finalCost)
+                .commission(commission);
+    }
+}
