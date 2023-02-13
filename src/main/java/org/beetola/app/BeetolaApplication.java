8e1bc9861eb9827fef805ef5ab963c60fa4b0ea9
Your Name <roma.grigoriev.2015@inbox.ru>
2023-02-13 23:34:08 +0400

    Enabled scheduling and conf props scan

--- src/main/java/org/beetola/app/BeetolaApplication.java
+++ src/main/java/org/beetola/app/BeetolaApplication.java
@@ -2,8 +2,13 @@ package org.beetola.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableScheduling
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BeetolaApplication {
 
     public static void main(String[] args) {
